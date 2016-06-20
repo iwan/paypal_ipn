a2ad7e3953423412d86b1706e9492f167b9e9ae4
Iwan Buetti <iwan.buetti@gmail.com>
2016-06-20 17:31:51 +0200

    added osme logs

--- server-alpha.rb
+++ server-alpha.rb
@@ -6,14 +6,14 @@ require 'http'
 post '/' do
   request.body.rewind
   response = HTTP.post("https://ipnpb.sandbox.paypal.com/cgi-bin/webscr", body: "cmd=_notify-validate&#{request.body.read}")
-  puts response.to_s
-  puts "----"
-  puts response.code
+  logger.info "response.to_s: #{response.to_s}"
+  logger.info "response.code: #{response.code}"
 end
 
 
 get "/hello" do
   puts "catch all [get]"
   puts params.inspect
+  logger.info "aaaaa"
   "Hello World!"
 end
